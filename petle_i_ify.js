5ae0d03cca81bcc3220600dba28147190de83ded
pikachu00XD <62176387+pikachu00XD@users.noreply.github.com>
2020-10-04 10:58:45 +0200

    Create petle_i_ify.js

---
+++ petle_i_ify.js
@@ -0,0 +1,18 @@
+//Zadanie 1
+
+var num1 = 63;
+var num2 = 45;
+
+if(num1 > num2) {console.log(num1);}
+else {console.log(num2);}
+
+
+//Zadanie 2
+
+var number1 = 5
+var number2 = 4
+var number3 = 3
+
+if(number1 > number2 && number1 > number3) {console.log(number1);}
+else if(number2 > number3) {console.log(number2);}
+else {console.log(number3)}
