bdffc42bc34e978b3e546377ad9f7993296f17c2
pikachu00XD <62176387+pikachu00XD@users.noreply.github.com>
2020-10-09 01:20:20 +0200

    Update petle_i_ify.js

--- petle_i_ify.js
+++ petle_i_ify.js
@@ -6,7 +6,6 @@ var num2 = 45;
 if(num1 > num2) {console.log(num1);}
 else {console.log(num2);}
 
-
 //Zadanie 2
 
 var number1 = 5
@@ -16,3 +15,25 @@ var number3 = 3
 if(number1 > number2 && number1 > number3) {console.log(number1);}
 else if(number2 > number3) {console.log(number2);}
 else {console.log(number3)}
+
+//Zadanie 3
+
+var x = 0;
+while(x<10)
+  {
+    console.log("Lubie JavaScript");
+    x++;
+  }
+
+//Zadanie 4
+
+var result = 0;
+var x = 1;
+while(x<=10)
+{
+  result += x;
+  x++;
+}
+console.log(result);
+
+//Zadanie 5
