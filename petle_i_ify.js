dea6910bc70e8080bdea9613c4e88d5c5aa4b770
pikachu00XD <62176387+pikachu00XD@users.noreply.github.com>
2020-10-15 02:32:02 +0200

    Update petle_i_ify.js

--- petle_i_ify.js
+++ petle_i_ify.js
@@ -68,3 +68,16 @@ for(var x = 0; x<=10; x++)
   }
 
 //Zadanie 7
+
+for(var x = 0; x<=100; x++)
+  {
+    if(x%3==0 && x%5==0){console.log("FizzBuzz")}
+    
+    else if(x%3==0){console.log("Fizz")}
+    
+    else if(x%5==0){console.log("Buzz")}
+    
+    else{console.log(x)}
+  }
+
+//Zadanie 8
